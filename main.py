ee0768d756dbc69e0e8f32a238bebf0e083052dd
plantago050956 <90820100+plantago050956@users.noreply.github.com>
2022-11-27 11:53:32 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 
 # ein Test
 print('Die Programmiersprache "Python" ist cool')
+
+# Variablen
+# was sind Variablem es sind Datenbehälter
+
+age = 66
+vorname = "Dieter"
+nachname = "Tholen"
+
+print("Anfang des Programms  " + __name__)
+
+print(age)
+
+print(vorname + ' ' + nachname)
+
+# Input
+name = input()
+
+test = input("Deine Eingabe: ")
+
+print("Ende des Programms" + " " + name)
